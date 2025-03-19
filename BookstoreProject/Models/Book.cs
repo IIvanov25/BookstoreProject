d82c8fe4f023135585c4acafcb1fc0ca25b991c9
IIvanov25 <iivanov250535@gmail.com>
2025-03-19 10:27:45 +0200

    Added models and properties

---
+++ BookstoreProject/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookstoreProject.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ BookstoreProject/Models/Review.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookstoreProject.Models
+{
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int Rating { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
