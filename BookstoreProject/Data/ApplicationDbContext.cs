2c596b4d82297b087534b7423f22c2a4d9cb346e
IIvanov25 <iivanov250535@gmail.com>
2025-03-19 10:42:23 +0200

    Changed page layout

--- BookstoreProject/Data/ApplicationDbContext.cs
+++ BookstoreProject/Data/ApplicationDbContext.cs
@@ -14,6 +14,5 @@ namespace BookstoreProject.Data
         public DbSet<Book> Books { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Review> Reviews { get; set; }
-        }
     }
 }
